7aabfa569d09ddd3346b10b751edde89e19c8df2
hongjie <32968210@qq.com>
2019-08-10 15:30:36 +0800

    模板

---
+++ src/models/login.js
@@ -0,0 +1,90 @@
+import { routerRedux } from 'dva/router';
+import { stringify } from 'qs';
+import { message } from 'antd';
+import { /* fakeAccountLogin, */ getFakeCaptcha } from '@/services/api';
+import { login } from '@/services/user';
+import { setAuthority } from '@/utils/authority';
+import { getPageQuery } from '@/utils/utils';
+import { reloadAuthorized } from '@/utils/Authorized';
+
+export default {
+  namespace: 'login',
+
+  state: {
+    status: undefined,
+  },
+
+  effects: {
+    *login({ payload }, { call, put }) {
+      const response = yield call(login, payload);
+      yield put({
+        type: 'changeLoginStatus',
+        payload: response.data,
+      });
+      // Login successfully
+      if (response.success) {
+        localStorage.setItem('token', response.data.token);
+        reloadAuthorized();
+        const urlParams = new URL(window.location.href);
+        const params = getPageQuery();
+        let { redirect } = params;
+        if (redirect) {
+          const redirectUrlParams = new URL(redirect);
+          if (redirectUrlParams.origin === urlParams.origin) {
+            redirect = redirect.substr(urlParams.origin.length);
+            if (window.routerBase !== '/') {
+              redirect = redirect.replace(window.routerBase, '/');
+            }
+            if (redirect.match(/^\/.*#/)) {
+              redirect = redirect.substr(redirect.indexOf('#') + 1);
+            }
+          } else {
+            redirect = null;
+          }
+        }
+        yield put(routerRedux.replace(redirect || '/'));
+      } else {
+        message.error(response.msg);
+      }
+    },
+
+    *getCaptcha({ payload }, { call }) {
+      yield call(getFakeCaptcha, payload);
+    },
+
+    *logout(_, { put }) {
+      yield put({
+        type: 'changeLoginStatus',
+        payload: {
+          status: false,
+          currentAuthority: 'guest',
+        },
+      });
+      localStorage.removeItem('token');
+      reloadAuthorized();
+      const { redirect } = getPageQuery();
+      // redirect
+      if (window.location.pathname !== '/user/login' && !redirect) {
+        yield put(
+          routerRedux.replace({
+            pathname: '/user/login',
+            search: stringify({
+              redirect: window.location.href,
+            }),
+          })
+        );
+      }
+    },
+  },
+
+  reducers: {
+    changeLoginStatus(state, { payload }) {
+      setAuthority(payload.currentAuthority);
+      return {
+        ...state,
+        status: payload.status,
+        type: payload.type,
+      };
+    },
+  },
+};
